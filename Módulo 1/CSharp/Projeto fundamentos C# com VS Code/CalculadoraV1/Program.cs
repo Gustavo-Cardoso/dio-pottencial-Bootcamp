90c203a58ccd5c0046be6152b5621814a252a5f9
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-09-29 20:43:43 -0300

    Exercício criação calculadora

---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/CalculadoraV1/Program.cs
@@ -0,0 +1,8 @@
+﻿using CalculadoraV1.obj.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 5);
+calc.Subtrair(10, 5);
+calc.Multiplicar(10, 5);
+calc.Dividir(10, 5);
---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/CalculadoraV1/readme.txt
@@ -0,0 +1,1 @@
+Criação de calculadora simples, utilizando C# com .NET
