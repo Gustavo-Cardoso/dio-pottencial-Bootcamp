fb0c744c87e87b3eded78cf9089c52f281bb2d6f
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-12 21:48:54 -0300

    Finalização aula sobre propriedades, métodos e contrutores no C#

---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/PropriedadeMetodoConstrutor/Program.cs
@@ -0,0 +1,21 @@
+﻿using PropriedadeMetodoConstrutor.Models;
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Gustavo";
+// p1.Sobrenome = "Cardoso";
+
+// Pessoa p2 = new Pessoa();
+// p2.Nome = "Débora";
+// p2.Sobrenome = "Lopes";
+
+// Usando os contrutores
+Pessoa p1 = new Pessoa(nome: "Gustavo", sobrenome: "Cardoso"); // Explicito
+Pessoa p2 = new Pessoa("Débora", "Lopes"); // Implícito
+
+Curso curso = new Curso();
+curso.Nome = "Inglês";
+curso.Alunos = new List<Pessoa>();
+
+curso.AdicionarAluno(p1);
+curso.AdicionarAluno(p2);
+curso.ListarAlunos();
