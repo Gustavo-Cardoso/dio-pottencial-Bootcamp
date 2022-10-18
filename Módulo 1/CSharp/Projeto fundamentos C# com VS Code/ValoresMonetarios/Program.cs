0365446e5da05584673448081c4715ccdb7084ac
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-18 20:50:51 -0300

    Aula sobre formatação monetária

---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
@@ -0,0 +1,8 @@
+﻿// Formatando valores monetários
+
+decimal valorMonetario = 1582.40M;
+
+Console.WriteLine($"{valorMonetario:C}"); 
+/* Quando uso interpolação e uso "dois pontos", isso signica que eu quero 
+formatar ele. Nesse caso o "C" ele formata em valor monetário de acordo com a configuração
+do sistema */
