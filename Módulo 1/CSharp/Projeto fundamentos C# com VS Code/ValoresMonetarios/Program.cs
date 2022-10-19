4aab0bbc58abf35b72940e8151697e8ef2dcf404
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-18 21:10:32 -0300

    Criando uma formatação personalizada

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
@@ -12,4 +12,9 @@ formatar ele. Nesse caso o "C" ele formata em valor monetário de acordo com a c
 do sistema */
 
 // Definindo uma cultura mesmo com a localização travada
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); // Formatando para real mesmo com a cultura travada em "en-Us"
+//Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); // Formatando para real mesmo com a cultura travada em "en-Us"
+
+// Criando uma formatação personalizada
+Console.WriteLine(valorMonetario.ToString("C1")); // 1 representa a quantidade de casas decimais
+Console.WriteLine(valorMonetario.ToString("C2")); // 2 representa a quantidade de casas decimais
+Console.WriteLine(valorMonetario.ToString("C8")); // 8 representa a quantidade de casas decimais
