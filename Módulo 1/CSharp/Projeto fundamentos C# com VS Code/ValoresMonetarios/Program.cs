b97a074c7993507d6789694f364cd441ceb5b1c0
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-18 21:33:31 -0300

    Formatando o tipo DateTime

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Globalization;
 
 // Alterando a localização do sistema para uma localização específica
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-Us"); // Formatando para dólar
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-Us"); // Formatando para dólar
 
 // Formatando valores monetários
-decimal valorMonetario = 1582.40M;
+// decimal valorMonetario = 1582.40M;
 
 //Console.WriteLine($"{valorMonetario:C}");
 /* Quando uso interpolação e uso "dois pontos", isso signica que eu quero 
@@ -20,11 +20,17 @@ do sistema */
 // Console.WriteLine(valorMonetario.ToString("C8")); // 8 representa a quantidade de casas decimais
 
 // Representado porcentagem
-double CEP = 17350000;
-
-Console.WriteLine(CEP.ToString("#####-###"));
+// double CEP = 17350000;
+// Console.WriteLine(CEP.ToString("#####-###"));
 
 // Criando uma formatação personalizada
-double porcentagem = .3421;
+// double porcentagem = .3421;
+// Console.WriteLine(porcentagem.ToString("P"));
 
-Console.WriteLine(porcentagem.ToString("P"));
+// Formatando o tipo DateTime
+DateTime data = DateTime.Now;
+// Apresentando somente dia/mês/ano hora:minuto:segundo
+Console.WriteLine(data);
+
+// Apresentando somente dia/mês/ano hora:minuto
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
