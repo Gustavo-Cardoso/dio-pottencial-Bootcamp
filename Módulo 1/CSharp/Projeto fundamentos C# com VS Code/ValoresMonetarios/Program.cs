b4802916f50658ba65a8ef1ad0d6f966cd826334
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-18 21:07:01 -0300

    Alterando a cultura "en-Us" para "pt-BR" mesmo com a "en-Us" como padrão

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
@@ -6,7 +6,10 @@ CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-Us"); // Formatand
 // Formatando valores monetários
 decimal valorMonetario = 1582.40M;
 
-Console.WriteLine($"{valorMonetario:C}");
+//Console.WriteLine($"{valorMonetario:C}");
 /* Quando uso interpolação e uso "dois pontos", isso signica que eu quero 
 formatar ele. Nesse caso o "C" ele formata em valor monetário de acordo com a configuração
 do sistema */
+
+// Definindo uma cultura mesmo com a localização travada
+Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); // Formatando para real mesmo com a cultura travada em "en-Us"
