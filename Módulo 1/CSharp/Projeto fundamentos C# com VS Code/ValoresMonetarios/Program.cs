65843d7419f87a19598e47e2dcba02c20f2675de
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-18 21:01:59 -0300

    Alterando a localização do sistema

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ValoresMonetarios/Program.cs
@@ -1,8 +1,12 @@
-﻿// Formatando valores monetários
+﻿using System.Globalization;
 
+// Alterando a localização do sistema para uma localização específica
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-Us"); // Formatando para dólar
+
+// Formatando valores monetários
 decimal valorMonetario = 1582.40M;
 
-Console.WriteLine($"{valorMonetario:C}"); 
+Console.WriteLine($"{valorMonetario:C}");
 /* Quando uso interpolação e uso "dois pontos", isso signica que eu quero 
 formatar ele. Nesse caso o "C" ele formata em valor monetário de acordo com a configuração
 do sistema */
