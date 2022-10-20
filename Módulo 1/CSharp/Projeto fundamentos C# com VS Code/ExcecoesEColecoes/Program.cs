6a10e21d932f30fe17d2b19e1e73c0c7d9ab32d6
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-19 21:47:44 -0300

    Capturando e tratando uma exceção

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ExcecoesEColecoes/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ExcecoesEColecoes/Program.cs
@@ -1,8 +1,16 @@
 ﻿// Realizando a leitura de um arquivo
 
-string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
-
-foreach (string linha in linhas)
+try
 {
-    Console.WriteLine(linha);
+    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
 }
