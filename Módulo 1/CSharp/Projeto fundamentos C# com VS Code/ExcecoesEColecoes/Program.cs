8eacc49235664ba14a96a231895071fb459bf80a
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-24 20:27:53 -0300

    Aprendendo a usar pilha

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ExcecoesEColecoes/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/ExcecoesEColecoes/Program.cs
@@ -31,4 +31,50 @@
 // }
 
 // Usando o Throw
-new ExemploExcecao().Metodo1();
+// new ExemploExcecao().Metodo1();
+
+// Usando FILA (FIFO -> First IN, First OUT -> Primeiro a entrar, primeiro a sair)
+
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Sempre irá remover o primeiro elemento que entrou na fila
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Usando Pilha (Last IN, Fist OUT -> Último a entrar, primeiro a sair)
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach (var item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+// Sempre irá remover o último elemento que entrou na pilha 
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+pilha.Push(20);
+
+foreach (var item in pilha)
+{
+    Console.WriteLine(item);
+}
