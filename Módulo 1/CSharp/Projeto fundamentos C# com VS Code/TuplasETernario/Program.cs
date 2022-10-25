93b4f5aec5195b7ebd24d8c6b71692b4ce0f0668
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-24 22:16:25 -0300

    Usando tupla em um método

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TuplasETernario/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TuplasETernario/Program.cs
@@ -1,16 +1,33 @@
-﻿using System.Globalization;
+﻿using TuplasETernario.Models;
+using System.Globalization;
 
 // Tupla
-(int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Gustavo", "Cardoso", 1.75M);// Essa aqui é a mais recomendada
+// (int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Gustavo", "Cardoso", 1.75M);// Essa aqui é a mais recomendada
 
 // Outras maneiras de usar a Tupla
 //Fazendo de uma dessas maneiras não é possível nomear meu objeto
 // ValueTuple<int, string, string, decimal> outroExemploTupla = (1, "Gustavo", "Cardoso", 1.75M);
 // var outroExemploTuplaCreate = Tuple.Create(1, "Gustavo", "Cardoso", 1.75M);
 
+// Console.WriteLine($"ID: {tupla.Item1}");
+// Console.WriteLine($"Nome: {tupla.Item2}");
+// Console.WriteLine($"Sobrenome: {tupla.Item3}");
+// Console.WriteLine($"Altura: {tupla.Item4}m");
 
-Console.WriteLine($"ID: {tupla.Item1}");
-Console.WriteLine($"Nome: {tupla.Item2}");
-Console.WriteLine($"Sobrenome: {tupla.Item3}");
-Console.WriteLine($"Altura: {tupla.Item4}m");
+// Usando uma tupla em um método
+LeituraArquivo arquivo = new LeituraArquivo();
 
+var (sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");
+
+if(sucesso)
+{
+    Console.WriteLine("Quantidade linhas do meu arquivo: " + quantidadeLinhas);
+    foreach (string linha in linhasArquivo)
+    {
+        Console.WriteLine(linha);
+    }
+}
+else
+{
+    Console.WriteLine("não foi porssível ler o arquivo");
+}
