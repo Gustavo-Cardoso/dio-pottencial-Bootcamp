0bc18f3cbd7d3ae8db181371f40919f55705abb3
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-10-24 21:30:57 -0300

    Aprendendo TUPLA

---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TuplasETernario/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+(int, string, string, decimal) tupla = (1, "Gustavo", "Cardoso", 1.75M);
+
+Console.WriteLine($"ID: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}m");
+
