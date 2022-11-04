dc5149cc6f48ec994aeb45f9c78f7d2a037c0123
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-11-04 11:09:04 -0300

    Aprendendo serialização

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NugetSerializarEAtributos.Models;
+using System.Globalization;
+using Newtonsoft.Json;
+
+Venda v1 = new Venda(1, "Material de escritório", 25.00M);
+
+//string serializado = JsonConvert.SerializeObject(v1);
+string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+
+Console.WriteLine(serializado);
