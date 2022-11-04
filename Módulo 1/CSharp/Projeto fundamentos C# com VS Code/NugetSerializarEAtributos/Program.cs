56d9a0b6d441d7522076fad4df654b8884d234ac
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-11-04 11:19:46 -0300

    Adição comentário sobre o Json

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 Venda v1 = new Venda(1, "Material de escritório", 25.00M);
 
-//string serializado = JsonConvert.SerializeObject(v1);
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+//string serializado = JsonConvert.SerializeObject(v1); // Aqui o Json é em uma única linha
+string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented); // Aqui fica com uma melhor visualização do Json, com as propriedades separadas por linha
 
 Console.WriteLine(serializado);
