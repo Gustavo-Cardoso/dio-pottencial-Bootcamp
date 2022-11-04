0aca745db10e8271dfe9d707e628200b097a6834
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-11-04 11:26:43 -0300

    Escrevendo um arquivo Json

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
@@ -7,4 +7,6 @@ Venda v1 = new Venda(1, "Material de escritório", 25.00M);
 //string serializado = JsonConvert.SerializeObject(v1); // Aqui o Json é em uma única linha
 string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented); // Aqui fica com uma melhor visualização do Json, com as propriedades separadas por linha
 
+File.WriteAllText("Arquivos/Vendas.json", serializado); // Crio um arquivo Json que recebe o conteúdo da variável serializado
+
 Console.WriteLine(serializado);
