5b70993ab9cf6556955d164d92a5394af636951f
Gustavo Cardoso <cardosogustavo790@gmail.com>
2022-11-08 07:14:52 -0300

    DateTime em JSON

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/NugetSerializarEAtributos/Program.cs
@@ -2,10 +2,12 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
+DateTime dataAtual = DateTime.Now; // Json usa como padrão no DateTime a iso 8601
+
 List<Venda> listaVendas = new List<Venda>();
 
-Venda v1 = new Venda(1, "Material de escritório", 25.00M);
-Venda v2 = new Venda(2, "Caderno 10 matérias capa dura ", 18.00M);
+Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Caderno 10 matérias capa dura ", 18.00M, dataAtual);
 
 listaVendas.Add(v1);
 listaVendas.Add(v2);
