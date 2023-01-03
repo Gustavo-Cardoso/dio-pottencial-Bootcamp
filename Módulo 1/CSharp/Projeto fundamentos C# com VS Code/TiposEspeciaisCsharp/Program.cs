aa6ad868ca45dee23dc07bbaba8ce8ad9df6f4f7
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-02 22:02:18 -0300

    Tipo Dinâmico

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using TiposEspeciaisCsharp.Models;
 using Newtonsoft.Json;
 
+/*--------------------- Nullable ---------------------------------*/
+
 // bool? desejaReceberEmail = true;
 
 // if (desejaReceberEmail.HasValue && desejaReceberEmail.Value)
@@ -12,7 +14,7 @@ using Newtonsoft.Json;
 //     Console.WriteLine("O usuário não respondeu ou optou por não receber e-mail.");
 // }
 
-/*------------------------------------------------------*/
+/*---------------------Nullable em propriedades ---------------------------------*/
 
 // string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
@@ -23,7 +25,7 @@ using Newtonsoft.Json;
 //     Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("d/MM/yyyy HH:mm")}, {(venda.Desconto.HasValue ? $"Desconto de : {venda.Desconto}" : "")}");
 // }
 
-/*------------------------------------------------------*/
+/*----------------------- Tipo Anônimo -------------------------------*/
 
 // var tipoAnonimo = new {Nome = "Gustavo", Sobrenome = "Cardoso", Altura = 1.77};
 
@@ -31,15 +33,26 @@ using Newtonsoft.Json;
 // Console.WriteLine("Sobrenome: " + tipoAnonimo.Sobrenome);
 // Console.WriteLine("Altura: " + tipoAnonimo.Altura);
 
-/*------------------------------------------------------*/
+/*-------------------------- Tipo Anônimo em coleção ----------------------------*/
 
-string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+// string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
-List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+// List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
-var listaAnonimo = listaVenda.Select(x => new {x.Produto, x.Preco});
+// var listaAnonimo = listaVenda.Select(x => new {x.Produto, x.Preco});
 
-foreach (var venda in listaAnonimo)
-{
-    Console.WriteLine($"Produto: {venda.Produto}, Preço: {venda.Preco}");
-}
+// foreach (var venda in listaAnonimo)
+// {
+//     Console.WriteLine($"Produto: {venda.Produto}, Preço: {venda.Preco}");
+// }
+
+/*-------------------------- Tipo Dinâmico ----------------------------*/
+
+dynamic variavelDinamica = 4;
+Console.WriteLine($"Tipo da variável: {variavelDinamica.GetType()}, Valor {variavelDinamica}");
+
+variavelDinamica = "texto";
+Console.WriteLine($"Tipo da variável: {variavelDinamica.GetType()}, Valor {variavelDinamica}");
+
+variavelDinamica = true;
+Console.WriteLine($"Tipo da variável: {variavelDinamica.GetType()}, Valor {variavelDinamica}");
