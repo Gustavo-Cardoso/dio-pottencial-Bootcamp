7417ed2bfd00cb0c46203da41fcdfee8422243de
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-02 21:31:32 -0300

    Tipo Anônimo

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
@@ -12,11 +12,17 @@ using Newtonsoft.Json;
 //     Console.WriteLine("O usuário não respondeu ou optou por não receber e-mail.");
 // }
 
-string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+// string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
-List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+// List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
-foreach (Venda venda in listaVenda)
-{
-    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("d/MM/yyyy HH:mm")}, {(venda.Desconto.HasValue ? $"Desconto de : {venda.Desconto}" : "")}");
-}
+// foreach (Venda venda in listaVenda)
+// {
+//     Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("d/MM/yyyy HH:mm")}, {(venda.Desconto.HasValue ? $"Desconto de : {venda.Desconto}" : "")}");
+// }
+
+var tipoAnonimo = new {Nome = "Gustavo", Sobrenome = "Cardoso", Altura = 1.77};
+
+Console.WriteLine("Nome: " + tipoAnonimo.Nome);
+Console.WriteLine("Sobrenome: " + tipoAnonimo.Sobrenome);
+Console.WriteLine("Altura: " + tipoAnonimo.Altura);
