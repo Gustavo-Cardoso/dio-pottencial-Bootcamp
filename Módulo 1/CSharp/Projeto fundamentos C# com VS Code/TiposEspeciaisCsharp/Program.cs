da5a094e87efa1130366c5c5c7c6016c06342f03
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-02 21:10:15 -0300

    nullable na prática

---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using TiposEspeciaisCsharp.Models;
+using Newtonsoft.Json;
+
+// bool? desejaReceberEmail = true;
+
+// if (desejaReceberEmail.HasValue && desejaReceberEmail.Value)
+// {
+//     Console.WriteLine("O usuário optou por receber e-mail.");
+// } 
+// else 
+// {
+//     Console.WriteLine("O usuário não respondeu ou optou por não receber e-mail.");
+// }
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("d/MM/yyyy HH:mm")}, {(venda.Desconto.HasValue ? $"Desconto de : {venda.Desconto}" : "")}");
+}
