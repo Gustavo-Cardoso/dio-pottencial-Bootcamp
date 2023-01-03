26e3a68a5c24fc6368e3cd9a13b98e51e7356425
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-02 21:42:52 -0300

    Tipo Anônimo em coleção

--- Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/TiposEspeciaisCsharp/Program.cs
@@ -12,6 +12,8 @@ using Newtonsoft.Json;
 //     Console.WriteLine("O usuário não respondeu ou optou por não receber e-mail.");
 // }
 
+/*------------------------------------------------------*/
+
 // string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
 // List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
@@ -21,8 +23,23 @@ using Newtonsoft.Json;
 //     Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("d/MM/yyyy HH:mm")}, {(venda.Desconto.HasValue ? $"Desconto de : {venda.Desconto}" : "")}");
 // }
 
-var tipoAnonimo = new {Nome = "Gustavo", Sobrenome = "Cardoso", Altura = 1.77};
+/*------------------------------------------------------*/
 
-Console.WriteLine("Nome: " + tipoAnonimo.Nome);
-Console.WriteLine("Sobrenome: " + tipoAnonimo.Sobrenome);
-Console.WriteLine("Altura: " + tipoAnonimo.Altura);
+// var tipoAnonimo = new {Nome = "Gustavo", Sobrenome = "Cardoso", Altura = 1.77};
+
+// Console.WriteLine("Nome: " + tipoAnonimo.Nome);
+// Console.WriteLine("Sobrenome: " + tipoAnonimo.Sobrenome);
+// Console.WriteLine("Altura: " + tipoAnonimo.Altura);
+
+/*------------------------------------------------------*/
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+var listaAnonimo = listaVenda.Select(x => new {x.Produto, x.Preco});
+
+foreach (var venda in listaAnonimo)
+{
+    Console.WriteLine($"Produto: {venda.Produto}, Preço: {venda.Preco}");
+}
