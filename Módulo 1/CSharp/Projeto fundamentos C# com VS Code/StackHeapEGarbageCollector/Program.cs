b54812f3050f398055c979d25c5af0e25e7fa7fd
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-04 20:44:49 -0300

    Memória Stack e Head

---
+++ Módulo 1/CSharp/Projeto fundamentos C# com VS Code/StackHeapEGarbageCollector/Program.cs
@@ -0,0 +1,34 @@
+﻿/*-------------------------------------------------------------------*/
+/*---------------------Alocação de memória---------------------------*/
+/*-------------------------------------------------------------------*/
+
+/*---------------------Stack e Heap---------------------------*/
+// Exemplo - Stack
+
+// int a = 5;
+
+// int b = 10;
+
+/*------------------------------------------------*/
+
+// Exemplo - Stack e Heap
+
+// Quando for objeto o valor vai ser criado e armazenado  no stack e uma referência (link) com a memória Heap
+
+// Pessoa p1 = new Pessoa();
+
+/*------------------Garbage Collector (GC)------------------------------*/
+
+// Todo objeto que está na heap e perdeu a referência com a stack (por que o método foi finalizado a excução) o GC mata o objeto, para limpar a memória
+
+/*---------------------------------------------------------------------------*/
+/*------------------Tipos de Valor e referência------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+/*------------------Tipos de Valor------------------------------*/
+
+// É uma variável que contém uma instância do tipo, ou seja ela armazena na memória stack e dentro dela ela contém o próprio valor também.
+
+/*------------------Tipos de referência------------------------------*/
+
+// É uma variável que contém referênci a uma instância do tipo, ou seja ele vai apontar um determinado endereço da memória heap.
