91d65c5890dd5e3d4c0911b994f1ba030891ef6b
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-10 22:12:15 -0300

    Encapsulamento na prática

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,8 +1,15 @@
 ﻿using POO.Models;
 
-Pessoa p1 = new Pessoa();
+// Pessoa p1 = new Pessoa();
 
-p1.Nome = "Gustavo";
-p1.Idade = 23;
+// p1.Nome = "Gustavo";
+// p1.Idade = 23;
 
-p1.Apresentar();
+// p1.Apresentar();
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
