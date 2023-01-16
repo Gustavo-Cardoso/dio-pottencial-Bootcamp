58c41d4470ea59ba7a21c43d07305e1ec1aeb29f
Gustavo Cardoso <50034636+Gustavo-Cardoso@users.noreply.github.com>
2023-01-16 16:08:44 -0300

    Herença na prática

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using POO.Models;
 
+/* Abstração */
 // Pessoa p1 = new Pessoa();
 
 // p1.Nome = "Gustavo";
@@ -7,9 +8,26 @@
 
 // p1.Apresentar();
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-c1.ExibirSaldo();
-c1.Sacar(500);
-c1.ExibirSaldo();
+/* Encapsulamento */
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
 
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
+
+
+/* Herança */
+
+Aluno a1 = new Aluno();
+a1.Nome = "Gustavo";
+a1.Idade = 23;
+a1.Email = "teste@teste.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Gustavo";
+p1.Idade = 23;
+p1.Salario = 2056.00M;
+p1.Apresentar();
